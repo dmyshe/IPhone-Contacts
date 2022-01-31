6e8f1e7c196c885b28ecc7a5600569748df34139
dmyshe <94231197+dmyshe@users.noreply.github.com>
2022-01-31 21:57:26 +0200

    Add  custom cell - UserAttributeViewCell.

---
+++ Contacts/Cells/UserAttributeViewCell.swift
@@ -0,0 +1,38 @@
+import Foundation
+import UIKit
+
+class UserAttributeViewCell : UITableViewCell {
+    
+    static let identifier = "UserAttributeViewCell"
+
+    // MARK: Views
+    var textField: UITextField = {
+        let textField = UITextField()
+        textField.textColor = .black
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: UserAttributeViewCell.identifier)
+        makeConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setPlaceholderText(_ text: String) {
+        textField.placeholder = text
+    }
+    
+    private func makeConstraints() {
+        contentView.addSubview(textField)
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constants.UI.Layout.defaultOffset),
+            textField.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -Constants.UI.Layout.defaultOffset),
+            textField.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: Constants.UI.Layout.defaultPadding),
+            textField.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -Constants.UI.Layout.defaultPadding)
+        ])
+    }
+}
