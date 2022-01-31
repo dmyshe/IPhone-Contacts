3e3b3025c60cec2fe6d2ef0fef366fae3ccf9fa1
dmyshe <94231197+dmyshe@users.noreply.github.com>
2022-01-31 23:18:10 +0200

    Add   3 button and titleLabel

--- Contacts/ContactsDetailScreen/ContactsDetailViewController.swift
+++ Contacts/ContactsDetailScreen/ContactsDetailViewController.swift
@@ -18,12 +18,62 @@ class ContactsDetailViewController: UIViewController {
         return imageView
     }()
     
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Test"
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     private lazy var editButton: UIBarButtonItem = {
         let editButton = UIBarButtonItem(title: LocalizeStrings.ContactsDetailViewController.edit,
                                          style: .plain,
                                          target: self,
                                          action: #selector(tapEditButton))
         return editButton
+    }()
+    
+    private lazy var messageButton: UIButton = {
+        let button = UIButton()
+        button.configure(title: "message", systemImage: "message.fill")
+        return button
+    }()
+    
+    private lazy var callButton: UIButton = {
+        let button = UIButton()
+        button.configure(title: "call", systemImage: "phone.fill")
+        return button
+    }()
+    
+    private lazy var mailButton: UIButton = {
+        let button = UIButton()
+        button.configure(title: "mail", systemImage: "envelope.fill")
+        return button
+    }()
+    
+    private lazy var titleBlockStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [contactImage,
+                                                      titleLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        stackView.distribution = .equalCentering
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    
+    private lazy var actionButtonStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [ messageButton,
+                                                        callButton,
+                                                        mailButton ])
+        stackView.axis = .horizontal
+        stackView.spacing = 4
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
    
     override func viewDidLoad() {
@@ -38,15 +88,25 @@ class ContactsDetailViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = false
         navigationItem.rightBarButtonItem = editButton
        
-        view.addSubview(contactImage)
+        view.addSubview(titleBlockStackView)
+        view.addSubview(actionButtonStackView)
     }
     
     private func makeConstraints() {
         NSLayoutConstraint.activate([
-            contactImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            contactImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleBlockStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleBlockStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
             contactImage.heightAnchor.constraint(equalToConstant: Constants.UI.Layout.contactImageHeight),
-            contactImage.widthAnchor.constraint(equalToConstant: Constants.UI.Layout.contactImageHeight)
+            contactImage.widthAnchor.constraint(equalToConstant: Constants.UI.Layout.contactImageHeight),
+            
+            actionButtonStackView.topAnchor.constraint(equalTo: titleBlockStackView.bottomAnchor, constant: Constants.UI.Layout.defaultPadding),
+            actionButtonStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: Constants.UI.Layout.defaultPadding),
+            actionButtonStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -Constants.UI.Layout.defaultPadding)
+            
+            
+            
+            
         ])
     }
     
