544d8cf6f68ec95964241fc848da112f6f90fdb2
dmyshe <94231197+dmyshe@users.noreply.github.com>
2022-02-18 20:20:31 +0200

    Add method addSubviewForAutoLayout to UIView

---
+++ Contacts/Utillity/Extensions.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+extension UIView {
+    func addSubviewForAutoLayout(_ view: UIView) {
+        self.addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
